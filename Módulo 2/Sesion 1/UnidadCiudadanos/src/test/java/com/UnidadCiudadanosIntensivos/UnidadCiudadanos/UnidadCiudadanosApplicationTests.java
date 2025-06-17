87c52fb060e678b0cd8229d32f24e31a63441247
Damaris <154326835+DamiMocch@users.noreply.github.com>
2025-06-16 23:21:15 -0600

    Add files via upload

---
+++ Módulo 2/Sesion 1/UnidadCiudadanos/src/test/java/com/UnidadCiudadanosIntensivos/UnidadCiudadanos/UnidadCiudadanosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UnidadCiudadanosIntensivos.UnidadCiudadanos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnidadCiudadanosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
